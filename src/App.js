2b953f157e6facd6e99fd551abfd5efe36ffd5e7
Tonyaap <stefan_kniest_@hotmail.com>
2020-10-12 14:51:00 +0200

    Set up react-router and skeletons for pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+import drumMachine from "./pages/drumMachine";
+import Nav from "./components/Nav";
+import Home from "./pages/home";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1> Hello Hello! </h1>
+      <Nav />
+      <h1>Welcome!</h1>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/drummachine" component={drumMachine} />
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/drumMachine/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function drumMachine() {
+  return (
+    <div>
+      <h1> DrumMachine! </h1>
+      <p> This is the DrumPage</p>
+    </div>
+  );
+}
+
+export default drumMachine;
