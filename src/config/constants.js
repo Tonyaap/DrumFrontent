844f988bb12f922bedb087eb9bc2c9133c7a98db
Stefan <69578301+Tonyaap@users.noreply.github.com>
2020-10-23 11:04:50 +0200

    Update constants.js

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,3 @@
-export const apiUrl = process.env.API_URL || "http://https://drumbackend.herokuapp.com/";
+export const apiUrl = process.env.API_URL || "https://drumbackend.herokuapp.com/";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
 export const DEFAULT_PAGINATION_LIMIT = 10;
