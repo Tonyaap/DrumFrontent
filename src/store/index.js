7d659904d54cbf57aa7a1138ad77be280e51f88b
Stefan <69578301+Tonyaap@users.noreply.github.com>
2020-10-23 14:13:14 +0200

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -3,8 +3,8 @@ import { createStore, applyMiddleware, compose } from "redux";
 import ReduxThunk from "redux-thunk";
 import reducer from "./rootReducer";
 
-const devTools =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+// const devTools =
+//  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
 const enhancer = compose(applyMiddleware(ReduxThunk), devTools);
 
