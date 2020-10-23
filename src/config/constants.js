f58c7d472fcea2cdf996fe96a88a956e1b856dfd
Tonyaap <stefan_kniest_@hotmail.com>
2020-10-23 12:06:08 +0200

    changed api url

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,7 @@
-export const apiUrl = process.env.API_URL || "https://drumbackend.herokuapp.com/";
+console.log("process.env", process.env);
+export const apiUrl =
+  process.env.NODE_ENV === "development"
+    ? "https://localhost:4000"
+    : "https://drumbackend.herokuapp.com";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
 export const DEFAULT_PAGINATION_LIMIT = 10;
