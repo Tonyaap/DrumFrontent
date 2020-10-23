888330e0e7a1035405ead791e1bb4dcd5f63791a
Stefan <69578301+Tonyaap@users.noreply.github.com>
2020-10-23 14:23:31 +0200

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -3,11 +3,16 @@ import { createStore, applyMiddleware, compose } from "redux";
 import ReduxThunk from "redux-thunk";
 import reducer from "./rootReducer";
 
-const devTools =
- window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+// const devTools =
+//  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store=createStore(rootreducer,
+    initialstate,
+    compose(applyMiddleware(...middleware))
+)
 
 const enhancer = compose(applyMiddleware(ReduxThunk), devTools);
 
-const store = createStore(reducer, enhancer);
+// const store = createStore(reducer, enhancer);
 
 export default store;
